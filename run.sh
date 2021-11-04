d3d4e7ce6616fba4080c3c6eb2b7fc2c6e265b7e
tomek <tomek.wlodarski@gmail.com>
2021-11-04 16:39:06 +0000

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
-# cryoBioEN map.mrc weight_of_1ake resolution noise
-
-python ~/git/cryoBioEN/cryoBioEN.py 4ake_10A.mrc 1 10 0.1
+# loop over all possible weights of 1ake.pdb
+for i in $(seq 0 0.1 1); do
+python ~/git/cryoBioEN/cryoBioEN.py 4ake_10A.mrc $i 10 0.1
+done
