3a103794996cbd0e23f02a05b19fb63f1d2dbbfd
tomek <tomek.wlodarski@gmail.com>
2022-11-15 17:21:15 +0000

    up

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ from os.path import exists
 import argparse
 
 
+import warnings
+warnings.filterwarnings('ignore')
+
 """
 "" PATHWAYS
 """
@@ -207,3 +210,75 @@ elif (mask == "sim"):
     sim_em_v_data=np.zeros([N_models,N_voxels])
     for i in range(0,N_models):
         sim_em_v_data[i]=sim_em_data[i][mask_comb]
+
+"""
+"" Optimization via Log-Weights as in Kofinger et. al 2019
+"""
+
+# To optimize the log-posterior under the constraints: sum of weights = 1 and weights >= 0 and to determine the optimal values of the weights by gradient-based minimization, we introduce log-weights: log_w = np.log(w)
+# They are determined up to an additive constant, which cancels during the normalization of the w.
+# Without loss of generality, because all w>0 we can set one of the log_w to 0. Then we would need to optimize log-posterior function for the remaining N-1 weights.
+
+"""
+"" Initial parameters, reference weights and log-weights
+"""
+
+w0,w_init,g0,g_init,sf_init = bioen_init_uniform(N_models)
+
+# std deviation, which is modelled from the noise distribution
+std = np.ones(N_voxels)*noise_thr_norm/3
+
+# Getting initial optimal scalling factor
+sf_start = leastsq(coeff_fit, sf_init, args=(w0,std,sim_em_v_data,exp_em_mask))[0]
+
+"""
+"" Parameters for optimization algorithm
+"""
+
+# For now we can only use BFGS algorithm as is coded in SCIPY
+
+epsilon = 1e-08
+pgtol = 1e-05
+maxiter = 5000
+
+# Number of iterations
+n_iter = 10
+
+# Theta
+thetas = [1000000,100000,10000,1000,100,10,1,0]
+
+# Running BioEN iterations through hyperparameter Theta:
+# w_opt_array, S_array, chisqrt_array = bioen(sim_em_v_data,exp_em_mask,std,thetas, g0, g_init, sf_start, n_iter, epsilon, pgtol, maxiter)
+
+# Running BioEN in a loop so we can apply knee dectection algorithm later
+w_opt_d = dict()
+sf_opt_d = dict()
+s_dict = dict()
+chisqrt_d = dict()
+for th in thetas:
+    w_temp, sf_temp = bioen_single(sim_em_v_data,exp_em_mask,std,th, g0, g_init, sf_start, n_iter, epsilon, pgtol, maxiter)
+    w_opt_d[th] = w_temp
+    sf_opt_d[th] = sf_temp
+    s_dict[th] = get_entropy(w0,w_temp)
+    chisqrt_d[th] = chiSqrTerm(w_temp,std,sim_em_v_data*sf_temp,exp_em_mask)
+
+# Knee locator used to find sensible theta value
+theta_index = knee_loc(list(s_dict.values()),list(chisqrt_d.values()))
+theta_knee = thetas[theta_index]
+theta_index_sort = theta_index
+
+for i in range(0,10):
+    thetas_old = np.sort(list(w_opt_d.keys()))[::-1]
+    theta_up = (thetas_old[theta_index_sort - 1] + thetas_old[theta_index_sort])/2.
+    theta_down = (thetas_old[theta_index_sort + 1] + thetas_old[theta_index_sort])/2.
+    for th in theta_up,theta_down:
+        w_temp, sf_temp = bioen_single(sim_em_v_data,exp_em_mask,std,th, g0, g_init, sf_start, n_iter, epsilon, pgtol, maxiter)
+        w_opt_d[th] = w_temp
+        sf_opt_d[th] = sf_temp
+        s_dict[th] = get_entropy(w0,w_temp)
+        chisqrt_d[th] = chiSqrTerm(w_temp,std,sim_em_v_data*sf_temp,exp_em_mask)
+        # Knee locator
+    theta_index_new = knee_loc(list(s_dict.values()),list(chisqrt_d.values()))
+    theta_new = list(w_opt_d.keys())[theta_index_new]
+    thetas_sorted = np.sort(list(w_opt_d.keys()))[::-1]
+    theta_index_sort = np.where(theta_new == thetas_sorted)[0][0]
